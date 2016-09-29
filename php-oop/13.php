0936dc06fe654d6aedabd9a4c705b41c730ae3b4
Xingzhang Lin <1129507496@qq.com>
2016-09-29 23:29:15 +0800

    add-1 php-jicheng

--- php-oop/13.php
+++ php-oop/13.php
@@ -17,8 +17,6 @@ class Human {
 
 class Stu extends Human {
 	public function subTell() {
-		var_dump($this);
-		die;
 		echo $this->wife;
 	}
 }
---
+++ php-oop/14.php
@@ -0,0 +1,36 @@
+<meta charset="UTF-8" />
+
+<?php
+
+class Human {
+	
+	private $wife = '小甜甜';
+	
+	public function cry() {
+		echo '555<br/>';
+	}
+	
+	public function pShow() {
+		echo $this->wife,'<br />'; // parent::pShow(); 中的 $this 中的 wife 有两个， 考虑执行上下文，只能获取到本类中。
+	}
+	
+}
+
+class Stu extends Human {
+	private $wife = 123;
+	
+	public $height = 180;
+	
+	public function subTell() {
+		parent::pShow();
+		echo $this->wife, '<br />';
+	}
+	
+}
+
+$li = new Stu();
+
+$li->subTell();
+
+?>
+
