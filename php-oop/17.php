7975add43077a0126f1e3ff21c66ed922f5aceef
Xingzhang Lin <1129507496@qq.com>
2016-10-10 23:48:17 +0800

    add gouzaohanshu

--- php-oop/17.php
+++ php-oop/17.php
@@ -12,6 +12,50 @@
 		
 	}
 	
+	class King extends  Human {
+		public function __construct() {
+			echo '<br />king';
+		} 
+	}
+	
 	$ming = new Stu();
+	$king = new King();
+	
+	class Mysql {
+		
+		protected $con = NULL;
+		
+		public function __construct() {
+			$this->con = mysql_connect('localhost', 'root', '');
+		}
+		
+		public function query( $sql ) {
+			return mysql_query($sql, $this->con);
+		}
+		
+	}
+	
+	class MyDb extends Mysql {
+		
+		public function __construct() {
+			// 如果子类继承时，有构造函数，保险一点
+			parent::__construct();
+			return true;
+			
+		}
+		
+		public function autoInsert() {
+			return $this->query('use test');
+		} 
+		
+	}
+	
+//	$mysql = new Mysql();
+//	var_dump($mysql->query('use test'));
+	
+	$mysql = new MyDb();
+	
+	var_dump($mysql->autoInsert());
+	
 	
 ?>
