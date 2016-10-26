0222595debe846dff984f199cf9fe69074f71b36
Xingzhang Lin <1129507496@qq.com>
2016-10-26 22:22:09 +0800

    add digui

--- recursion/02.php
+++ recursion/02.php
@@ -1,8 +1,10 @@
 <?php
 	
+//	模仿`tree /f`命令
+	
 	// 打印级联目录
 	
-	function recdir( $path ) {
+	function recdir( $path, $lev = 1 ) {
 		
 		$dh = opendir($path);
 		
@@ -11,18 +13,18 @@
 			if ( $row == '.' || $row == '..' ) {
 				continue;
 			}
-			 
-			echo $row,'<br />';
 			
+			echo '|-',str_repeat('-', $lev),$row,'<br />';
 			
 			// 判断给定文件名是否是一个目录
 			if ( is_dir($path. '/' . $row) ) {
-				recdir($path. '/' . $row);
+				recdir($path. '/' . $row, $lev + 1);
 			}
 			
 		}
 		closedir($dh); 
 	} 
 	
-	recdir('H:\360downloads');	
+	recdir('H:\360downloads', 1);
+		
 ?>
---
+++ recursion/tree.js
@@ -0,0 +1,41 @@
+
+var fs = require('fs');
+
+//function recdir( $path ) {
+//	
+//	var $dh = 
+//	
+//}
+//
+//recdir
+
+fs.readdir('H:/360downloads', function ( err, files ) {
+	
+	console.log( files );
+	
+});
+//
+//function recdir( $path ) {
+//	
+//	fs.readdir($path, function ( err, files ) {
+//		
+//		console.log( files );
+//		
+///*		files.forEach(function ( file ) {
+//			
+//			fs.exists($path + '/' + file, function ( exists ) {
+//				
+//				if ( exists ) {
+//					recdir($path);
+//				}
+//				
+//			});
+//			
+//		});
+//		*/
+//	});
+////	fs.close();
+//	
+//}
+//
+//recdir('H:\360downloads');
