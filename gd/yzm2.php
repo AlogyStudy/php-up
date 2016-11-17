6bb99c990b15b5dd2cbd9003e04d14289d050fea
Xingzhang Lin <1129507496@qq.com>
2016-11-18 00:00:51 +0800

    change gb

--- gd/yzm2.php
+++ gd/yzm2.php
@@ -1,13 +1,16 @@
 <?php
 
 	// 创建画布
-	$im = imagecreatetruecolor(100, 20);
+	$im = imagecreatetruecolor(50, 20);
 	
 	// 颜色
 	$red = imagecolorallocate($im, 200, 10, 10);
 	
 	// 画
+	$str = 'abcdefghjkmnpqrstuvwxyzABCDEFGHJKMNPQRSTUVWXYZ23456789';
+	$str = substr(str_shuffle($str), 0, 4);
 	
+	imagestring($im, 5, 10, 2, $str, $red);
 			
 	// 输出
 	header("Content-type: image/png");
